b1f8941e2c2dbfa3605ea049e4df91cdda91cd62
Carson Levine <carsonglevine@me.com>
2020-10-09 16:25:05 -0400

    valgrind fix

--- valgrind.sh
+++ valgrind.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#valgrind --leak-check=full --show-leak-kinds=all test_put                                          
+valgrind --leak-check=full --show-leak-kinds=all test_put                                          
                                                                                
-#valgrind --leak-check=full --show-leak-kinds=all test_concat
+valgrind --leak-check=full --show-leak-kinds=all test_concat
 
-#valgrind --leak-check=full --show-leak-kinds=all test_close
+valgrind --leak-check=full --show-leak-kinds=all test_close
 
 valgrind --leak-check=full --show-leak-kinds=all test_remove
 
-#valgrind --leak-check=full --show-leak-kinds=all test_search
+valgrind --leak-check=full --show-leak-kinds=all test_search
 
-#valgrind --leak-check=full --show-leak-kinds=all test_get
+valgrind --leak-check=full --show-leak-kinds=all test_get
 
-#valgrind --leak-check=full --show-leak-kinds=all test_open
+valgrind --leak-check=full --show-leak-kinds=all test_open
