513a9c6e24cab1b12045116953265388f82dabb5
Carson Levine <carsonglevine@me.com>
2020-12-08 14:57:38 -0500

    superfasthash separate file

--- valgrind.sh
+++ valgrind.sh
@@ -4,7 +4,7 @@ valgrind --leak-check=full --show-leak-kinds=all test_hsearch
 
 valgrind --leak-check=full --show-leak-kinds=all test_hremove
 
-valgrind --leak-check=full --show-leak-kinds=all test_put
+#valgrind --leak-check=full --show-leak-kinds=all test_put
 
 valgrind --leak-check=full --show-leak-kinds=all test_hopen
 
@@ -12,14 +12,14 @@ valgrind --leak-check=full --show-leak-kinds=all test_hput
 
 valgrind --leak-check=full --show-leak-kinds=all test_happly
                                                                                
-valgrind --leak-check=full --show-leak-kinds=all test_concat
+# valgrind --leak-check=full --show-leak-kinds=all test_concat
 
-valgrind --leak-check=full --show-leak-kinds=all test_close
+# valgrind --leak-check=full --show-leak-kinds=all test_close
 
-valgrind --leak-check=full --show-leak-kinds=all test_remove
+# valgrind --leak-check=full --show-leak-kinds=all test_remove
 
-valgrind --leak-check=full --show-leak-kinds=all test_search
+# valgrind --leak-check=full --show-leak-kinds=all test_search
 
-valgrind --leak-check=full --show-leak-kinds=all test_get
+# valgrind --leak-check=full --show-leak-kinds=all test_get
 
-valgrind --leak-check=full --show-leak-kinds=all test_open
+# valgrind --leak-check=full --show-leak-kinds=all test_open
